bb917daba119af3f7cd52236751e5e6126421486
Sahil Gupte <ovenoboyo@gmail.com>
2023-12-21 15:44:33 +0530

    Initial build

--- python-src/main.py
+++ python-src/main.py
@@ -1,73 +1,111 @@
 #!/usr/bin/env python3
 
 import asyncio
-from moosyncLib.lib import start, event_handler
-from moosyncLib.data import Song
+import sys
+from moosyncLib.lib import connect_client, start, event_handler
 from moosyncLib.api import api
-from moosyncLib.data import ContextMenuItem
+from moosyncLib.data import ContextMenuItem, ExtensionPreferenceGroup
 
+import typing
 from urllib.parse import urlparse, parse_qs
 from yt_dlp import YoutubeDL
 import os
 
+__dirname = sys.argv[1]
+default_store_path = os.path.join(__dirname, "downloads")
+
+
 def video_id(value):
     query = urlparse(value)
-    if query.hostname == 'youtu.be':
+    if query.hostname == "youtu.be":
         return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
+    if query.hostname in ("www.youtube.com", "youtube.com"):
+        if query.path == "/watch":
             p = parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
+            return p["v"][0]
+        if query.path[:7] == "/embed/":
+            return query.path.split("/")[2]
+        if query.path[:3] == "/v/":
+            return query.path.split("/")[2]
     return None
 
+
 async def on_started():
-    await api.set_context_menu_item([ContextMenuItem(type="SONGS", label="Try download", handler=context_menu_handler)])
-    await api.set_context_menu_item([ContextMenuItem(type="PLAYLIST_CONTENT", label="Try download", handler=context_menu_handler)])
-    
-async def context_menu_handler(songs: list[dict]):
-    urls = []
-    s = []
-    if len(songs) > 0:
+    print("Started ytdlp")
+    await api.set_context_menu_item(
+        [
+            ContextMenuItem(
+                type="SONGS", label="Try download", handler=context_menu_handler
+            )
+        ]
+    )
+    await api.set_context_menu_item(
+        [
+            ContextMenuItem(
+                type="PLAYLIST_CONTENT",
+                label="Try download",
+                handler=context_menu_handler,
+            )
+        ]
+    )
+
+    await api.add_user_preferences(
+        ExtensionPreferenceGroup(
+            key="path",
+            title="Download path",
+            default=default_store_path,
+            type="FilePicker",
+        )
+    )
+
+
+async def context_menu_handler(songs: typing.Optional[list[dict]]):
+    if songs is not None and len(songs) > 0:
+        urls = []
+        s = []
+        store_path = await api.get_preferences("path")
+        if store_path is None:
+            store_path = default_store_path
         for song in songs:
             if song["type"] == "YOUTUBE" and song["playbackUrl"] is not None:
                 if song["playbackUrl"].startswith("https://"):
-                    urls.append(song["playbackUrl"])
+                    urls.append(
+                        f"https://www.youtube.com/watch?v={video_id(song['playbackUrl'])}"
+                    )
                 else:
-                    urls.append(f"https://www.youtube.com/watch?v={song['playbackUrl']}")
-                    
+                    urls.append(
+                        f"https://www.youtube.com/watch?v={song['playbackUrl']}"
+                    )
+
                 s.append(song)
 
-    ydl_opts = {
-        'format': 'm4a/bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'm4a',
-        }],
-        "paths": { 
-            "home": "/home/ovenoboyo/ytdl"
-        },
-        "outtmpl": {
-            "default": "%(id)s"
+        ydl_opts = {
+            "format": "m4a/bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "m4a",
+                }
+            ],
+            "paths": {"home": store_path},
+            "outtmpl": {"default": "%(id)s"},
         }
-    }
-    with YoutubeDL(ydl_opts) as ydl:
-        await api.show_toast("Downloading songs", 3000, "info")
-        i = 0
-        for url in urls:
-            error = ydl.download(url)
-            if error == 0:
-                print(video_id(url), f"/home/ovenoboyo/ytdl/{video_id(url)}", flush=True)
-                s[i]["path"] = f"/home/ovenoboyo/ytdl/{video_id(url)}.m4a"
-                s[i]["type"] = "LOCAL"
-                await api.update_song(s[i])
-            print( "error code: ", error, flush=True)
-            i += 1
+
+        with YoutubeDL(ydl_opts) as ydl:
+            await api.show_toast("Downloading songs", 3000, "info")
+            i = 0
+            for url in urls:
+                error = ydl.download(url)
+                if error == 0:
+                    s[i]["path"] = os.path.join(store_path, f"{video_id(url)}.m4a")
+                    await api.update_song(s[i])
+                    await api.show_toast(f"Downloaded {s[i]['title']}", 1000, "info")
+                i += 1
+
 
 async def main():
+    connect_client()
+    print("Starting extension")
     await event_handler.add_listener("onStarted", on_started, [])
     await start()
 
--- python-src/setup.py
+++ python-src/setup.py
@@ -6,8 +6,5 @@ setup(
     packages=["moosyncLib"],
     py_modules=[""],
     scripts=["main.py"],
-    install_requires=[
-        'yt_dlp',
-    ],
+    install_requires=["Brotli==1.1.0", "certifi==2023.7.22", "mutagen==1.47.0", "pex==2.1.148", "pycryptodomex==3.19.0", "websockets==12.0", "yt-dlp==2023.10.13", ""]
 )
-    
